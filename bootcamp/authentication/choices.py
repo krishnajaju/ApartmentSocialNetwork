48fe6e18c4c43796f93ebf1c3790f738ebd7ea21
Krishna Jaju <krishna.jaju24@gmail.com>
2016-11-05 04:51:17 +0530

    Initial commit

---
+++ bootcamp/authentication/choices.py
@@ -0,0 +1,26 @@
+RESIDENT = 0
+GATEKEEPER = 1
+
+TYPE_CHOICES = (
+    (RESIDENT, "Resident"),
+    (GATEKEEPER, "Gatekeeper")
+)
+
+
+STUDENT = 1
+BUSINESS = 2
+HOUSEWIFE = 3
+SELF_EMPLOYED = 4
+PVT_JOB = 5
+GOVT_JOB = 6
+OTHER = 7
+
+JOB_CHOICES = (
+    (STUDENT, "Student"),
+    (BUSINESS, "Business"),
+    (HOUSEWIFE, "Housewife"),
+    (SELF_EMPLOYED, "Self Employed"),
+    (PVT_JOB, "Private Job"),
+    (GOVT_JOB, "Government Job"),
+    (OTHER, "Other"),
+)
